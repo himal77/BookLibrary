824bdb8f6c0ab999b11169907b29fc8748ea4645
himal77 <himalpuri6@gmail.com>
2020-07-07 22:29:04 +0200

    Created Home Page with a navigation using bootstrap

---
+++ src/main/java/com/mountech/MountechApplication.java
@@ -0,0 +1,11 @@
+package com.mountech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountechApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MountechApplication.class, args);
+	}
+}
