897aa5b25003b27cb59c38c63db061da1c1e8035
himal77 <himalpuri6@gmail.com>
2020-07-11 18:01:16 +0200

    Added my account page

--- src/main/java/com/mountech/controller/HomeController.java
+++ src/main/java/com/mountech/controller/HomeController.java
@@ -7,4 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
     public String index(){ return "index"; }
+
+    @RequestMapping("/myAccount")
+    public String myAccount(){return "myAccount";}
 }
