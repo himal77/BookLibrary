398428ba4ade3ebc76cfcb0567ef289a1f2955b0
himal77 <himalpuri6@gmail.com>
2020-08-05 18:58:37 +0200

    Created profile functionality and page

--- src/main/java/com/mountech/controller/HomeController.java
+++ src/main/java/com/mountech/controller/HomeController.java
@@ -1,9 +1,16 @@
 package com.mountech.controller;
 
+import com.mountech.domain.User;
 import com.mountech.domain.security.PasswordResetToken;
+import com.mountech.service.UserService;
+import com.mountech.service.impl.UserSecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,6 +18,13 @@ import java.util.Locale;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserSecurityService userSecurityService;
+
     @RequestMapping("/")
     public String index(){ return "index"; }
 
@@ -21,18 +35,36 @@ public class HomeController {
     }
 
     @RequestMapping("/forgetPassword")
-    public String forgetPassword(Model model,  Locale locale,
-                                 @RequestParam("token") String token,
-                                 Model model){
-        PasswordResetToken passToken = userService.getPasswordResetToken(token);
-
+    public String forgetPassword(Model model){
         model.addAttribute("classActiveForgetPassword", true);
         return "myAccount";
     }
 
     @RequestMapping("/newAccount")
-    public String newUser(Model model){
-        model.addAttribute("classActiveNewAccount", true);
-        return "myAccount";
+    public String newUser(
+            Locale locale,
+            @RequestParam("token") String token,
+            Model model){
+
+        PasswordResetToken passToken = userService.getPasswordResetToken(token);
+
+        if(passToken == null){
+            String message = "Invalid Token.";
+            model.addAttribute("message", message);
+            return "redirect: /badRequest";
+        }
+
+        User user = passToken.getUser();
+        String username = user.getUsername();
+
+        UserDetails userDetails = userSecurityService.loadUserByUsername(username);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(),
+                userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        model.addAttribute("classActiveEdit", true);
+        return "myProfile";
     }
 }
